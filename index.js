bb2629ab3dff8f4a797e24bdabc00fc6eb35b5f6
avciemre <avci_em@hotmail.com>
2020-12-14 18:51:32 +0100

    setting life value

--- index.js
+++ index.js
@@ -49,8 +49,13 @@ const ATTACK_VALUE = 10; //uppercase just to show its a constant
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
-let chosenMaxLife = 100;
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+}
+
+let chosenMaxLife = parseInt(enteredValue);
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
